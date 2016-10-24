bdcea862e7de54c3e5c5f4254f67d0cd90fee2b3
James Christopher Adduono <jc@adduono.com>
2016-10-24 00:55:45 -0400

    LG: Update MobiCore driver -> Trustonic TEE
    Change-Id: Ie4daac3008eb812ba495abc90764c57ff1ee4246

--- drivers/gud/MobiCoreDriver/mci/mcitime.h
+++ drivers/gud/MobiCoreDriver/mci/mcitime.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015 TRUSTONIC LIMITED
+ * Copyright (c) 2015 TRUSTONIC LIMITED
  * All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or
@@ -11,20 +11,17 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  */
-#ifndef _MC_LINUX_API_H_
-#define _MC_LINUX_API_H_
 
-#include <linux/types.h>
-
-/*
- * Switch TEE active core to core_num, defined as linux
- * core id
- */
-int mc_switch_core(int core_num);
+#ifndef MCITIME_H_
+#define MCITIME_H_
 
 /*
- * Return TEE active core as Linux core id
+ * Trustonic TEE RICH OS Time:
+ * Seconds and nanoseconds since Jan 1, 1970, UTC
  */
-int mc_active_core(void);
+struct mcp_time {
+	u64	seconds;
+	u64	nsec;
+};
 
-#endif /* _MC_LINUX_API_H_ */
+#endif /* MCITIME_H_ */
